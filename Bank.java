8492b06e4b86991d517ee29090946c780bae5e8a
Avi bansal <33563429+avibansal@users.noreply.github.com>
2024-05-16 15:45:38 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,21 +1,69 @@
 package Bank;
 
-public class Date {
-	private int day,month,year;
-
-	public Date(int day, int month, int year) {
-		this.day = day;
-		this.month = month;
-		this.year = year;
+public class Bank {
+	private int id;
+	private String name;
+	private String email;
+	private int balance;
+	private Date db1;
+	private static int count;
+	
+	static {
+		count=100;
 	}
-	public Date() {
-		this.day = 1;
-		this.month = 19;
-		this.year = 1890;
+	
+	public void display_all() {
+		System.out.println(id+" "+name+" "+email+" "+db1+" "+balance);
 	}
 	public String toString() {
-		return day+"/"+month+"/"+year;
+		return "id:"+id+"  name:"+name+"  email:"+email+"  DOB:"+db1+"  balance:"+balance;
 	}
 	
+	public Bank() {
+//		System.out.println("Default constructor invoke");
+		this.id=count;
+		this.name="ram";
+		this.email="ram@gmail.com";
+		this.balance=10000;
+		this.db1=new Date();
+		count+=1;
+	}
+	public Bank(String name,String email,Date db1,int balance) {
+		this.id=count;
+		this.name=name;
+		this.email=email;
+		this.balance=balance;
+		this.db1=db1;
+//		System.out.println("Parameter constructor invoke");
+		count+=1;
+	}
 	
+	public void transfer(Bank tr,int money) {
+		this.balance-=money;
+		tr.balance+=money;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setname(String name) {
+		this.name=name;
+	}
+	public void setemail(String email) {
+		this.email=email;
+	}
+	public void setbalance(int balance) {
+		this.balance=balance;
+	}
+	
+
+	public String getname() {
+		return this.name;
+	}
+	public String getemail() {
+		return this.email;
+	}
+	public int getbalance() {
+		return this.balance;
+	}
 }
